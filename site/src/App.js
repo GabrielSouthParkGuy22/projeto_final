c75e7363c0cd61e6281d15a988cc3c41d2126c4f
Luan Reinhold <anjobranco67@gmail.com>
2023-11-24 19:23:45 -0300

    add tela de login do Usuario

--- site/src/App.js
+++ site/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import './index.css'
 
 import Rodape from './components/rodape';
+import PageUserLogin from './pages/usuarioLogin'
 function App() {
   return (
     <div className="App">
-      <Rodape/>
+      <PageUserLogin/>
     </div>
   );
 }
