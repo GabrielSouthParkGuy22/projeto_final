49f29fe8a84a40b2d67c8f27f25e6b0fb6eb8cab
GabrielSouthParkGuy22 <145403361+GabrielSouthParkGuy22@users.noreply.github.com.>
2023-12-06 18:59:15 -0300

    header component and toggler component

--- site/src/App.js
+++ site/src/App.js
@@ -1,13 +1,14 @@
+import "./App.css";
+import "./index.css";
 
-import './App.css';
-import './index.css'
-
-import Rodape from './components/rodape';
-import PageUserLogin from './pages/usuarioLogin'
+import Rodape from "./components/rodape";
+import Header from "./components/header/header";
+import Toggler from "./components/toggler/toggler";
+import PageUserLogin from "./pages/usuarioLogin";
 function App() {
   return (
     <div className="App">
-      <PageUserLogin/>
+      <PageUserLogin />
     </div>
   );
 }
